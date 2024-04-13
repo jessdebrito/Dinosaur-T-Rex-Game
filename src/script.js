9fd1140b9c3cbb482898debdd79454b711b62f6f
jessdebrito <debrito.herself@gmail.com>
2024-04-13 12:09:19 -0300

    jump

--- src/script.js
+++ src/script.js
@@ -1,3 +1,50 @@
 const BACKGROUND = document.querySelector(".background")
 const DIVSCORE = document.querySelector(".score");
 const DIVHIGHSCORE = document.querySelector(".highScore");
+
+class Dino {
+    #dinoHTML;
+    #isJumping = false;
+    #dinoPosition = 0;
+
+
+    constructor(divDino) {
+        this.#dinoHTML = divDino;
+    };
+
+    getDinoPosition() { 
+        let dinoPosition = this.#dinoPosition;
+        return dinoPosition;
+    };
+
+    getIsJumping() {
+        let isJumping = this.#isJumping;
+        return isJumping;
+    }
+
+    jump(){
+        this.#isJumping = true;
+        let dinoHTML = this.#dinoHTML;
+
+        let upInterval = setInterval(() => {
+            if(this.#dinoPosition >= 150){
+                clearInterval(upInterval);
+
+                let downInterval = setInterval(() =>{
+                    if(this.#dinoPosition === 0){
+                        clearInterval(downInterval)
+                        this.#isJumping = false;
+                    } else {
+                        this.#dinoPosition -= 20;
+
+                        dinoHTML.style.bottom = `${this.#dinoPosition}px`;
+                    }
+                }, 25 + 9.8)
+            } else {
+                this.#dinoPosition += 20;
+
+                dinoHTML.style.bottom = `${this.#dinoPosition}px`;
+            }
+        }, 25 + 9.8);
+
+    };
